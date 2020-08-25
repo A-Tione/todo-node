fda38720f71e22c94cfc42c37cb3c164315f31be
A-Tione <heyanggo@gmail.com>
2020-08-25 20:12:38 +0800

    add tests

---
+++ __tests__/db.spec.js
@@ -0,0 +1,27 @@
+const db  = require('../db')
+const fs = require('fs')
+jest.mock('fs')
+
+describe('db', ()=> {
+  afterEach(()=> {
+    fs.clearMocks()
+  })
+})
+
+it ('can read', async () => {
+  const data = [{title: 'hi', done: true}]
+  fs.setReadFileMock('/xxx', null, JSON.stringify(data))
+  const list = await db.read('/xxx')
+  expect(list).toStrictEqual(data)
+})
+
+it ('can write', async () => {
+  let fakeFile
+  fs.setWriteFileMock('/yyy', (path, data, callback) => {
+    fakeFile = data
+    callback(null)
+  })
+  const list = [{title: '吃冰棒', done: true}, {title: '吃火锅', done: true}]
+  await db.write(list, '/yyy')
+  expect(fakeFile).toBe(JSON.stringify(list) + '\n')
+})
