9b8f9a8e323fe5d528dda368a7de775022aaac4a
A-Tione <heyanggo@gmail.com>
2020-09-01 20:22:08 +0800

    add tests

--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -1,27 +1,27 @@
-const db  = require('../db')
+const db = require('../db')
 const fs = require('fs')
 jest.mock('fs')
 
-describe('db', ()=> {
+describe('bd', ()=> {
   afterEach(()=> {
     fs.clearMocks()
   })
 })
 
-it ('can read', async () => {
+it('can read', async() => {
   const data = [{title: 'hi', done: true}]
   fs.setReadFileMock('/xxx', null, JSON.stringify(data))
   const list = await db.read('/xxx')
   expect(list).toStrictEqual(data)
-})
+});
 
-it ('can write', async () => {
+it('can write', async () => {
   let fakeFile
   fs.setWriteFileMock('/yyy', (path, data, callback) => {
     fakeFile = data
     callback(null)
   })
-  const list = [{title: '吃冰棒', done: true}, {title: '吃火锅', done: true}]
+  const list =  [{title: '吃冰淇淋', done: true}, {title: '吃火锅', done: true}]
   await db.write(list, '/yyy')
   expect(fakeFile).toBe(JSON.stringify(list) + '\n')
-})
+});
